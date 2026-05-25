29d4555841cbc8183cabf0776cbf7de450260f59
odmoreno81-ux <odmoreno81@gmail.com>
2026-05-25 14:17:35 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-1779645030855-qawodz9od">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Guía didáctica | UNA MIRADA AL PASADO</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Unidad de Trabajo:
+Estudio de los estilos de la peluquería en las diferentes décadas del S.XX. En  esta unidad se trabajarán las técnicas propias de cada época así como la herramientas y cosméticos utilizados.">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/image-gallery/image-gallery.js"></script><link rel="stylesheet" href="../idevices/image-gallery/image-gallery.css">
+<script src="../idevices/image-gallery/simple-lightbox.min.js"></script><link rel="stylesheet" href="../idevices/image-gallery/simple-lightbox.min.css">
+<script src="../libs/exe_lightbox/exe_lightbox.js"> </script>
+<link rel="stylesheet" href="../libs/exe_lightbox/exe_lightbox.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-1779645030855-qawodz9od" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node no-ch">Introducción</a>
+</li>
+<li> <a href="../html/tarea-21-una-mirada-al-pasado.html" class="no-ch">Tarea 2.1. Una mirada al pasado</a>
+</li>
+<li> <a href="../html/tarea-71-linea-temporal-del-proyecto.html" class="no-ch">Tarea 7.1 Línea temporal del proyecto</a>
+</li>
+<li> <a href="../html/tarea-41-descripcion-del-producto-final.html" class="no-ch">Tarea 4.1 Descripción del producto final</a>
+</li>
+<li> <a href="../html/curacion-de-contenidos.html" class="daddy">Curación de contenidos</a>
+<ul class="other-section">
+<li> <a href="../html/tarea-51-herramientas-utilizadas-por-el-alumnado.html" class="no-ch">Tarea 5.1 Herramientas utilizadas por el alumnado</a>
+</li>
+<li> <a href="../html/tarea-51-herramientas-utilizadas-por-el-profesor.html" class="no-ch">Tarea 5.1 Herramientas utilizadas por el profesor</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/tarea-23-derechos-digitales.html" class="no-ch">Tarea 2.3 Derechos digitales</a>
+</li>
+<li> <a href="../html/evaluacion.html" class="daddy">Evaluación</a>
+<ul class="other-section">
+<li> <a href="../html/tarea-61-criterios-de-evaluacion-asociados.html" class="no-ch">Tarea 6.1 Criterios de evaluación asociados</a>
+</li>
+<li> <a href="../html/tarea-81-instrumentos-de-evaluacion.html" class="no-ch">Tarea 8.1 Instrumentos de evaluación</a>
+</li>
+</ul>
+</li>
+<li class="active"> <a href="../html/guia-didactica.html" class="active daddy">Guía didáctica</a>
+<ul class="other-section">
+<li> <a href="../html/creditos.html" class="no-ch">Créditos</a>
+</li>
+</ul>
+</li>
+</ul>
+</nav><main id="page-1779645030855-qawodz9od" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">UNA MIRADA AL PASADO</p>
+<p class="package-subtitle">Historia de la Peluquería</p></div>
+<div class="page-header"><h1 class="page-title">Guía didáctica</h1></div>
+</header><div id="page-content-page-1779645030855-qawodz9od" class="page-content">
+<article id="block-1779645272759-njbuya84x" class="box">
+<header class="box-head no-icon">
+<h1 class="box-title">Criterios de calificaci</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1779645272758-tvy5gwvia" class="idevice_node image-gallery" data-idevice-path="../idevices/image-gallery/" data-idevice-type="image-gallery" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1779645272758-tvy5gwvia&quot;,&quot;img_0&quot;:{&quot;img&quot;:&quot;../content/resources/Guía didáctica.png&quot;,&quot;thumbnail&quot;:&quot;../content/resources/Guía didáctica.png&quot;,&quot;title&quot;:&quot;&quot;,&quot;linktitle&quot;:&quot;&quot;,&quot;author&quot;:&quot;&quot;,&quot;linkauthor&quot;:&quot;&quot;,&quot;license&quot;:&quot;&quot;}}" data-idevice-template="image-gallery.html">
+<!-- Idevice Example template start -->
+<div class="exe-image-gallery-template">
+  <!-- Idevice content start -->
+  
+            <div class="imageGallery-IDevice">
+                <div class="imageGallery-body"><div id="imageContainer_0" class="imageContainer"> <a idevice-id="idevice-1779645272758-tvy5gwvia" title="" href="../content/resources/Guía didáctica.png" class="imageLink">  <div class="imageElement">   <img src="../content/resources/Guía didáctica.png" height="128" width="128" title="" alt="" titlelink="" author="" authorlink="" license="" licenselink=""/>  </div> </a></div>
+                </div>
+            </div>
+  <!-- Idevice content end -->
+</div>
+<!-- Idevice Example template end -->
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/tarea-81-instrumentos-de-evaluacion.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/creditos.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class=""> <p> <span class="license-label">Licencia: </span><span class="license">Dominio público</span></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
